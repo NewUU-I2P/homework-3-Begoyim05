5da5e571c6a1aad9169451d40a5dfe70794ffeb0
Begoyim05 <147371694+Begoyim05@users.noreply.github.com>
2023-11-10 16:37:32 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,6 +1,25 @@
-void problemSolution1(float consumed_water) {
-    float cost;
-    // write your code here
+#include <iostream>
+using namespace std;
 
-    return cost;
+int main() {
+    float cost=0;
+    cout<<" how many cubic meters of water? \n";
+    cin>>cost;
+    if (cost>30){
+        if (cost>50){
+            if (cost>60){
+                cost=13+0.4*30+0.12*20+14+(cost-60)*1.5;
+            }
+            else{
+                cost=13+0.4*30+0.12*20+(cost-50)*1.4;
+            }
+        }
+        else{
+            cost=13+0.4*30+0.12*(cost-30);
+        }
+    }
+    else{
+        cost=cost*0.4+13;
+    }
+    cout<<cost<<endl;
 }
