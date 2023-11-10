2ed097829ac86f404eac844becbdbad022da92b3
Begoyim05 <147371694+Begoyim05@users.noreply.github.com>
2023-11-10 17:11:00 +0500

    Update problem_3.cpp

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -1,8 +1,34 @@
-#include <string>
+#include <iostream>
+using namespace std;
 
-std::string problemSolution3(float height, char S) {
-    // write your code here
+int main() {
+    char sex;
+    float height;
+    cout<<" what is your sex? (in capital letters) ";
+    cin>>sex;
+    cout<<" what is your height?(in meters) ";
+    cin>>height;
+    if (sex=='M'){
+        if (height<1.70) {
+            cout<<"you are short\n ";
+        }
+        if (height>=1.70 && height<1.85){
+            cout<<"you are normal\n";
+        }
+        if(height>1.85){
+            cout<<"you are tall\n";
+        }
+    }
+    if (sex=='F'){
+        if (height<1.60) {
+            cout<<"you are short\n ";
+        }
+        if (height>=1.60 && height<1.75){
+            cout<<" you are normal\n";
+        }
+        if (height>1.75){
+            cout<<"you are tall\n";
+        }
 
-    // use return to return your result
-    // make use of control flow statements
+    }
 }
