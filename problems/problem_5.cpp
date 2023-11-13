8dcf7ae25529fd46d00db37a26ef437cb613bd85
Begoyim05 <147371694+Begoyim05@users.noreply.github.com>
2023-11-13 16:14:20 +0500

    Update problem_5.cpp

--- problems/problem_5.cpp
+++ problems/problem_5.cpp
@@ -1,6 +1,30 @@
-float problemSolution5(float x, float y, char operation) {
-   float result;
-   // write your code here
+#include <iostream>
+using namespace std;
 
-   return result;
+int main(){
+    float m=0;
+    int n=0;
+    char o;
+    cout<<"first number=";
+    cin>>m;
+    cout<<"second number=";
+    cin>>n;
+    cout<<"what arithmetic operation ";
+    cin>>o;
+    switch(o) {
+        case '/':
+            m /= n;
+            break;
+        case '*':
+            m *= n;
+            break;
+        case '+':
+            m += n;
+            break;
+        case '-':
+            m -= n;
+            break;
+    }
+    cout<<m<<endl;
 }
+
