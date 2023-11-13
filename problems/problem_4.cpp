b6b903bc67024e413c8ff7e26c3c8cfd24c6b6cb
Begoyim05 <147371694+Begoyim05@users.noreply.github.com>
2023-11-13 16:40:15 +0500

    Update problem_4.cpp

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -1,9 +1,39 @@
 #include <string>
-#include <sstream>
+#include <iostream>
+using namespace std;
 
-std::string problemSolution4(const std::string &macAddress) {
-    // write your code here
-
-    // make use of control flow statements
-    // return result;
+int main(){
+    string addres;
+    int g;
+    cin>>addres;
+    if (addres=="FFFFFFFFFFFF"){
+        cout<<" this address is broadcast\n";
+        exit(0);
+    }
+    char l=addres[0];
+    char m=addres[1];
+    if ( isdigit(addres.at(0)) ) {
+        l -= 48;
+    }
+    else{
+        l -= 55;
+    }
+    if ( isdigit(addres.at(1)) ) {
+        m -= 48;
+    }
+    else{
+        m-=55;
+    }
+    g=(l*16+m)%2;
+    switch(g){
+        case 0:
+            cout<<" this address is unicast\n";
+            break;
+        case 1:
+            cout<<" this address is multicast\n";
+            break;
+    }
 }
+
+
+
